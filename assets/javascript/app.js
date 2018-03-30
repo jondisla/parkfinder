43b86e71a64c114dafb44a35e848d4b2c22e3528
Giovanna <giovanna.perina@hotmail.com>
2018-03-30 18:53:02 -0400

    -Add more files, images. -Create the `main.css` file and `app.js` file. -Add a few functionalities on `javascript`.

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+ 
+    // Init Tooltip
+    $('.tooltipped').tooltip();
+
+    // When button clicked save on favorites
+   $(".heart").click(function(){
+    M.toast({html: 'Park Saved!', classes:'rounded'});
+   });
+   
+        
+    $('.park-content').css({
+    display: 'none'
+    });
+
+    $("#search-parks, #all-parks").click(function() {
+    event.preventDefault()
+        $(".main").hide();
+        $(".park-content").show()
+        $('html,body').animate({
+            scrollTop: $(".parks-content").offset().top},
+            'slow');
+});
+
+
+});
